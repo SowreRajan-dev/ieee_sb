a9e16ec0aa24113736221962a2281b8cd5763435
Sowrerajan <sowrerajanofficial@gmail.com>
2022-08-19 21:01:47 +0530

    ham burger and responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 function Navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
   return (
-    <NavbarContainer>
+    <NavbarContainer clicked={navbarOpen}>
       <NavbarLeft>
         <IeeeLogo>
           <IEEEAndSairam
@@ -13,24 +17,29 @@ function Navbar() {
         </IeeeLogo>
       </NavbarLeft>
       <NavbarMiddle>
-        <NavbarLink>
-          <NavbarLinkText>Home</NavbarLinkText>
-        </NavbarLink>
-        <NavbarLink>
-          <NavbarLinkText>About Us</NavbarLinkText>
-        </NavbarLink>
-        <NavbarLink>
-          <NavbarLinkText>Society</NavbarLinkText>
-        </NavbarLink>
-        <NavbarLink>
-          <NavbarLinkText>Members</NavbarLinkText>
-        </NavbarLink>
-        <NavbarLink>
-          <NavbarLinkText>Events</NavbarLinkText>
-        </NavbarLink>
-        <NavbarLink>
-          <NavbarLinkText>Gallery</NavbarLinkText>
-        </NavbarLink>
+        <MenuLabel htmlFor="navi-toggle" onClick={handleToggle}>
+          <Icon clicked={navbarOpen}>&nbsp;</Icon>
+        </MenuLabel>
+        <NavbarUl clicked={navbarOpen}>
+          <NavbarLink>
+            <NavbarLinkText>Home</NavbarLinkText>
+          </NavbarLink>
+          <NavbarLink>
+            <NavbarLinkText>About Us</NavbarLinkText>
+          </NavbarLink>
+          <NavbarLink>
+            <NavbarLinkText>Society</NavbarLinkText>
+          </NavbarLink>
+          <NavbarLink>
+            <NavbarLinkText>Members</NavbarLinkText>
+          </NavbarLink>
+          <NavbarLink>
+            <NavbarLinkText>Events</NavbarLinkText>
+          </NavbarLink>
+          <NavbarLink>
+            <NavbarLinkText>Gallery</NavbarLinkText>
+          </NavbarLink>
+        </NavbarUl>
       </NavbarMiddle>
       <NavbarRight>
         <ContactUsContainer>
@@ -50,12 +59,31 @@ const NavbarContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  @media screen and (max-width: 950px) {
+    ${(props) => {
+      if (props.clicked) {
+        return `
+            height: 100vh;
+            width: 100vw;
+            background: #fff;
+            align-items: center;
+            justify-content: space-between;
+            flex-direction: column;
+            `;
+      }
+    }}
+  }
 `;
 
 const NavbarLeft = styled.div`
   flex: 1;
   height: 100%;
   padding: 10px 0;
+  @media screen and (max-width: 950px) {
+    flex: 0;
+    margin-bottom: 20px;
+  }
 `;
 
 const NavbarMiddle = styled.div`
@@ -65,6 +93,60 @@ const NavbarMiddle = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 10px 0;
+  position: relative;
+
+  @media screen and (max-width: 950px) {
+    align-items: center;
+    justify-content: center;
+    flex: 2;
+  }
+`;
+
+const MenuLabel = styled.label`
+  background-color: #ffffff;
+  position: fixed;
+  top: 10px;
+  right: 10px;
+  border-radius: 10px;
+  height: 60px;
+  width: 60px;
+  cursor: pointer;
+  z-index: 1000;
+  box-shadow: 0 1rem 3rem rgba(182, 237, 200, 0.3);
+  text-align: center;
+
+  @media (min-width: 950px) {
+    display: none;
+  }
+`;
+
+const Icon = styled.span`
+  position: relative;
+  background-color: ${(props) => (props.clicked ? "transparent" : "black")};
+  width: 2rem;
+  height: 2px;
+  display: inline-block;
+  margin-top: 2rem;
+  transition: all 0.3s;
+  &::before,
+  &::after {
+    content: "";
+    background-color: black;
+    width: 2rem;
+    height: 2px;
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    transition: all 0.3s;
+  }
+  &::before {
+    top: ${(props) => (props.clicked ? "0" : "-0.8rem")};
+    transform: ${(props) => (props.clicked ? "rotate(135deg)" : "rotate(0)")};
+  }
+  &::after {
+    top: ${(props) => (props.clicked ? "0" : "0.8rem")};
+    transform: ${(props) => (props.clicked ? "rotate(-135deg)" : "rotate(0)")};
+  }
 `;
 
 const NavbarRight = styled.div`
@@ -73,6 +155,10 @@ const NavbarRight = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 950px) {
+    display: none;
+  }
 `;
 
 const IeeeLogo = styled.div``;
@@ -82,8 +168,33 @@ const IEEEAndSairam = styled.img`
   cursor: pointer;
 `;
 
-const NavbarLink = styled.div`
+const NavbarLink = styled.li`
   padding: 10px;
+  list-style: none;
+`;
+
+const NavbarUl = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+
+  @media (max-width: 950px) {
+    display: ${(props) => (props.clicked ? "flex" : "none")};
+    transition: all 0.3s ease-in-out;
+
+    ${(props) => {
+      if (props.clicked) {
+        return `
+          background: #fff;
+          flex-direction: column;
+          align-items: center;
+           
+    `;
+      }
+    }}
+  }
 `;
 
 const NavbarLinkText = styled.p`
