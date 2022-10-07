0c21595a89a689c7c63a5d5f9cbab82431603d19
arunmanikandan <106937963+arunman1kandan@users.noreply.github.com>
2022-10-07 10:15:50 +0530

    Added Navbar Functionality
    Added a new feature where in the navbar changes from transparent to white

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useState , Fragment } from "react";
 import styled from "styled-components";
 
 function Navbar() {
+
+  const [colorChange, setColorchange] = useState(false);
+  const changeNavbarColor = () =>{
+     if(window.scrollY >= 80){
+       setColorchange(true);
+     }
+     else{
+       setColorchange(false);
+     }
+  };
+  window.addEventListener('scroll', changeNavbarColor);
   return (
+    <Fragment>
+      <Nav className={colorChange ? 'navbar colorChange' : 'navbar'}>
     <NavbarContainer>
       <NavbarLeft>
         <IeeeLogo>
@@ -38,15 +52,31 @@ function Navbar() {
         </ContactUsContainer>
       </NavbarRight>
     </NavbarContainer>
+    </Nav>
+    </Fragment>
   );
 }
 
 export default Navbar;
 
+
+export const Nav = styled.nav`
+   background: transparent;
+   height: 80px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   font-size: 1rem;
+   /* Fix your navbar by using below two lines of code */
+   position: sticky;
+   top:0;
+   /* Fix your navbar by using above two lines of code */
+   z-index: 10;
+`
 const NavbarContainer = styled.div`
   height: 90px;
   width: 100%;
-  background-color: #fff;
+  background-color: transparent;
   display: flex;
   justify-content: space-between;
   align-items: center;
